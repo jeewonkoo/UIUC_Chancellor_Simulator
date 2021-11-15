f17f0379ffc7959e0ed0543cdc04dfecb12ecf31
Ayush Sharma <41262335+AyushSharma173@users.noreply.github.com>
2021-11-15 10:24:49 -0600

    Added struct classes for event and faculty(still need modification)

---
+++ UIUC_Chancellor_Simulator/Faculty.hpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Faculty {
+
+	string name;
+	vector<string> title;
+	int fundingRequired;	//increment or decrement by event 
+    int reputation;		//stat for now 
+    int student_life;
+};
