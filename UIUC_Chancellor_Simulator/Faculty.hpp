51e56691a06ca87434684489945c9bea941058ae
Ayush Sharma <41262335+AyushSharma173@users.noreply.github.com>
2021-11-15 10:26:49 -0600

    Corrected format and indentation

--- UIUC_Chancellor_Simulator/Faculty.hpp
+++ UIUC_Chancellor_Simulator/Faculty.hpp
@@ -3,10 +3,9 @@
 using namespace std;
 
 struct Faculty {
-
-	string name;
-	vector<string> title;
-	int fundingRequired;	//increment or decrement by event 
+    string name;
+    vector<string> title;
+    int fundingRequired;	//increment or decrement by event 
     int reputation;		//stat for now 
     int student_life;
 };
