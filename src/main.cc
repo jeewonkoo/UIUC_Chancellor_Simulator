998d8078a9f8cbbcfbb7fe1d43cf1163f34d4079
TingfengLuo <tluo9@illinois.edu>
2021-12-04 12:29:11 +0000

    console setup

--- src/main.cc
+++ src/main.cc
@@ -10,15 +10,27 @@
 #include </usr/include/cppconn/statement.h>
 using namespace std;
 int main() {
-Database* d = new Database();
-Event* g = d->getEventbyID(1);
-Event* f = d->getRandomEvent();
+Database* database = new Database();
+Event* curr = database->getRandomEvent();
 
+School* school = new School();
 
-School* s = new School();
-std::cout << s->GetReputation();
-std::cout << f->id;
-std::cout << g->id;
+while (curr&&school->GetFunding()>=0&&school->GetReputation()>=0&&school->GetStudent_life()>=0) {
+    school->Decide(curr);
+    if (curr->AddRemove) {
+        bool add = true;
+        for (int a=0; a<school->GetFacultySize(); ++a) {
+            if (curr->faculty==school->GetFaculty()[a]) {
+                school->RemoveFaculty(curr->faculty);
+                add = false;
+            }
+        }
+        if (add) {
+            school->AddFaculty(curr->faculty);
+        }
+    }
+    curr = database->getRandomEvent();
+}
 
 return 0;
     }
